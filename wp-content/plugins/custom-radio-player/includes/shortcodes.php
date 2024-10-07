8300026ea4902fce540df74aae6aa0446a0d4744
santiagoavilez <santiagolingreed@gmail.com>
2024-10-07 18:05:35 -0300

    chore: Update radio player image dimensions in shortcode

--- wp-content/plugins/custom-radio-player/includes/shortcodes.php
+++ wp-content/plugins/custom-radio-player/includes/shortcodes.php
@@ -74,7 +74,7 @@ function radio_player_shortcode($atts)
             <p class="radio-catch-mobile" style="font-size: 1.25rem;">Ahora:</p>
 
             <div class="radio-image">
-                <img width="320" height="320" src="<?php echo esc_url($imagen); ?>" alt="Imagen del programa">
+                <img width="480" height="480" src="<?php echo esc_url($imagen); ?>" alt="Imagen del programa">
                 <button class="button-audio-mobile" onclick='togglePlayPause("https://cdn.instream.audio/:9366/stream")'>
                     <svg id="playIconCumbre" xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24" fill="currentColor" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-play icon-player  active ">
                         <polygon points="6 3 20 12 6 21 6 3" />
