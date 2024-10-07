4ab052e338a392644f7588e76fb78e1227c29179
santiagoavilez <santiagolingreed@gmail.com>
2024-10-07 18:06:13 -0300

    Update radio player image dimensions in shortcode back to 100%

--- wp-content/plugins/custom-radio-player/includes/shortcodes.php
+++ wp-content/plugins/custom-radio-player/includes/shortcodes.php
@@ -236,8 +236,8 @@ function radio_player_shortcode($atts)
             }
 
             .radio-image img {
-                width: 480px;
-                height: 480px;
+                width: 100%;
+                height: 100%;
                 object-fit: cover;
                 border-radius: 0.5rem;
 
